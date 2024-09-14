f626a7f7323a5f87948abc6ee5d755026745b7f4
Jhonattan Benitez Ballesteros <40676789+jhonattanbenitez@users.noreply.github.com>
2024-09-14 08:51:27 -0500

    remove unused vars

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
